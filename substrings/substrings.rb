6b083a4dcb36b0bfc27a026bf605ad19d0da8a1e
Eric Romano <ecr1980@gmail.com>
2022-02-14 12:22:39 -0500

    program now counts total iterations of input string. Also prints out each dictionary word and test word as a test.

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -1,5 +1,30 @@
 dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "i", "low", "own", "part", "partner", "sit"]
 puts "Please enter a string, try to use some of the words listed in the default dictionary!"
 puts dictionary
-userEntry = gets
-puts userEntry
+user_entry = gets.chomp
+string_user_entry = user_entry.split("")
+print string_user_entry
+
+
+
+def substrings(user_input,stored_list)
+  sum = 0
+  
+    for word in stored_list do
+      
+        word_array = word.split("")
+        x = word_array.length()
+        user_input.each_with_index do |element, index|
+          new_word = user_input[index, x]
+          if new_word == word_array
+            puts "got one!"
+            sum = sum + 1
+          end
+          p new_word
+          p word
+        end
+    end
+  puts sum
+end
+
+substrings(string_user_entry, dictionary)
