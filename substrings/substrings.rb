cbf59bdf655de2c135bc43b3e9293abe1fe7678e
Eric Romano <ecr1980@gmail.com>
2022-02-14 10:08:58 -0500

    Saved a user input with a test output.

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -1,1 +1,5 @@
 dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "i", "low", "own", "part", "partner", "sit"]
+puts "Please enter a string, try to use some of the words listed in the default dictionary!"
+puts dictionary
+userEntry = gets
+puts userEntry
